3d10c0a69c35bdb7fd7118d6a75f9c75432d8050
matgr <matheus.gonca.mgr@gmail.com>
2017-12-19 15:51:46 -0200

    Basic spawn system

---
+++ LightYearStar/Assets/Scripts/Spawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public bool stage1 = false;
+    public bool stage2 = false;
+    public bool stage3 = false;
+    public bool stage4 = false;
+
+    public float distance = 0f;
+
+    public float spawnTime = 3f;
+    public GameObject enemy;
+
+    private float sizeX, sizeY;
+    private float screenRatio = (float)Screen.width / (float)Screen.height;
+    // Use this for initialization
+    void Start ()
+    {
+        sizeX = Camera.main.orthographicSize * screenRatio;
+        sizeY = Camera.main.orthographicSize;
+
+        StartCoroutine(SpawnRot());
+    }
+
+    IEnumerator SpawnRot()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            AddBasicEnemy();
+        }
+    }
+    
+    void AddBasicEnemy()
+    {
+        Instantiate(enemy, new Vector2(Random.Range(sizeX-0.5f, -sizeX+0.5f), sizeY+0.6f), Quaternion.identity);
+    }      
+
+    // Update is called once per frame
+    void Update ()
+    {
+        distance = Time.deltaTime + distance;
+        if ((distance > 20)&&(stage1==false))
+        {
+            spawnTime = spawnTime - 0.2f;
+            stage1 = true;
+        }
+        else
+            if ((distance > 50) && (stage2 == false))
+        {
+            spawnTime = spawnTime - 0.3f;
+            stage2 = true;
+        }
+        else
+            if ((distance > 70) && (stage3 == false))
+        {
+            spawnTime = spawnTime - 0.5f;
+            stage3 = true;
+        }
+        else
+            if ((distance > 100) && (stage4 == false))
+        {
+            spawnTime = spawnTime - 0.5f;
+            stage4 = true;
+        }
+    }
+}
